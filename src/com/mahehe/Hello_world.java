c4a0e1eabb4a7b56a4b3b17e9c806b8af0d1047d
maheheliting <maheheliting@t.shu.edu.cn>
2017-03-22 23:00:43 +0800

    mahehe,test

--- src/com/mahehe/Hello_world.java
+++ src/com/mahehe/Hello_world.java
@@ -6,5 +6,6 @@ package com.mahehe;
 public class Hello_world {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("qiuqiu");
     }
 }
